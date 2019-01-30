6872f048d258fd1869bad1712a65c986108bc932
k7996g <35267235+k7996g@users.noreply.github.com>
2019-01-30 13:59:07 +0530

    Update SortAndReverse.java

--- lab7/src/com/cg/lab7/bean/SortAndReverse.java
+++ lab7/src/com/cg/lab7/bean/SortAndReverse.java
@@ -1,15 +1,24 @@
 package com.cg.lab7.bean;
 import java.util.*;
 public class SortAndReverse {
-         public void getSorted(int a[])
-         {ArrayList<Integer> s=new ArrayList<Integer>();
+         public int[] getSorted(int a[])
+         {ArrayList<String> s=new ArrayList<String>();
           int i;
+          int b[]=new int[a.length];
           for(i=0;i<a.length;i++)
-        	  s.add(a[i]);
+          { String s1=Integer.toString(a[i]);
+          StringBuilder s2=new StringBuilder(s1);
+          s2.reverse();
+          String s3=new String(s2);
+          s.add(s3);
+          }
           Collections.sort(s);
-          Collections.reverse(s);
-          Iterator j=s.iterator();
+          String c[]=s.toArray(new String[0]);
+          for(i=0;i<c.length;i++)
+          {b[i]=Integer.parseInt(c[i]);}
+          return b;
+         /* Iterator j=s.iterator();
           while(j.hasNext())
-        	  System.out.println(j.next());
+        	  System.out.println(j.next());*/
          }
 }
