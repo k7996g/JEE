620083d2f413a47f7932cf67b4af880eb1c8d755
k7996g <35267235+k7996g@users.noreply.github.com>
2019-01-31 14:27:05 +0530

    Update Factorial.java

--- Practice/lab 8.3/src/com/cg/lab8/bean/Factorial.java
+++ Practice/lab 8.3/src/com/cg/lab8/bean/Factorial.java
@@ -5,11 +5,12 @@ public class Factorial extends Thread{
       static int c=0;
       static int f=1;
       static int x;
+      Random rand=new Random();
       public void run()
       {if(c!=1)
-      {Random rand=new Random();
-       x=rand.nextInt(10000)+1;c++;
-       
+      {
+       x=rand.nextInt(10)+1;c++;
+       System.out.println(x);
       }
       else
       {for(int i=1;i<=x;i++)
