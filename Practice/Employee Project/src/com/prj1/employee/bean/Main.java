000da0ddf8e7d0c465551b346a6a094ca5ea2c8e
k7996g <35267235+k7996g@users.noreply.github.com>
2019-02-02 15:31:38 +0530

    Update Main.java

--- Practice/Employee Project/src/com/prj1/employee/bean/Main.java
+++ Practice/Employee Project/src/com/prj1/employee/bean/Main.java
@@ -4,14 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		AccSch a = new AccSch();
-		AccSch b = new AccSch();
-		System.out.println(a.createAccount(100, "kaushal", "12/10/2015", 'e', "Engg", 50000, 20000, 20000));
-		System.out.println(b.createAccount(101, "keshav", "12/10/2016", 'm', "manager", 70000, 22000, 18000));
-		a.setDaAndDesignation('e');
-		b.setDaAndDesignation('m');
-		a.showDetails(100);
-		b.showDetails(101);
+		AccSch a[] = new AccSch[4];
+		a[0] = new AccSch();
+		a[1] = new AccSch();
+		System.out.println(a[0].createAccount(Integer.parseInt(args[0]), "kaushal", "12/10/2015", 'e', "Engg", 50000,
+				20000, 20000));
+
+		a[0].setDaAndDesignation('e');
+		a[0].showDetails(Integer.parseInt(args[0]));
+		System.out.println(
+				a[1].createAccount(Integer.parseInt(args[1]), "keshav", "12/10/2016", 'm', "Man", 70000, 22000, 18000));
+		a[1].setDaAndDesignation('m');
+		a[1].showDetails(Integer.parseInt(args[1]));
+		System.out.println(a[1].salary(Integer.parseInt(args[1])));
+		new AccSch().showDetails(102);
 	}
 
 }
