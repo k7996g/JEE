8df01e7b3a090b7c0a3cafb312a2c3803cc2c22a
k7996g <35267235+k7996g@users.noreply.github.com>
2019-02-07 08:20:05 -0800

    Update AccountService.java

--- Exam Question/src/com/cg/mra/service/AccountService.java
+++ Exam Question/src/com/cg/mra/service/AccountService.java
@@ -1,9 +1,10 @@
 package com.cg.mra.service;
 
 import com.cg.mra.bean.Account;
+import com.cg.mra.exception.PhoneNumberNotFoundException;
 
 public interface AccountService {
-	Account getAccountDetails(String mobileNo);
+	Account getAccountDetails(String mobileNo) throws PhoneNumberNotFoundException;
 
-	double rechargeAccount(String mobileNo, double rechargeAmount);
+	double rechargeAccount(String mobileNo, double rechargeAmount) throws PhoneNumberNotFoundException;
 }
