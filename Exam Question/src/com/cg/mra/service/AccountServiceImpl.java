1f82d09138431236febb39bfb0310e8d9ddeccf2
k7996g <35267235+k7996g@users.noreply.github.com>
2019-02-07 08:20:39 -0800

    Update AccountServiceImpl.java

--- Exam Question/src/com/cg/mra/service/AccountServiceImpl.java
+++ Exam Question/src/com/cg/mra/service/AccountServiceImpl.java
@@ -3,18 +3,19 @@ package com.cg.mra.service;
 import com.cg.mra.bean.Account;
 import com.cg.mra.dao.AccountDao;
 import com.cg.mra.dao.AccountDaoImpl;
+import com.cg.mra.exception.PhoneNumberNotFoundException;
 
 public class AccountServiceImpl implements AccountService {
 	Account account = new Account();
 	AccountDao dao = new AccountDaoImpl();
 
-	public AccountServiceImpl(String mobileNo) {
+	public AccountServiceImpl(String mobileNo) throws PhoneNumberNotFoundException {
 		getAccountDetails(mobileNo);
 
 	}
 
 	@Override
-	public Account getAccountDetails(String mobileNo) {
+	public Account getAccountDetails(String mobileNo) throws PhoneNumberNotFoundException {
 		account = dao.getAccountDetails(mobileNo);
 		System.out.println(account.getAccountBalance());
 		return account;
@@ -22,7 +23,7 @@ public class AccountServiceImpl implements AccountService {
 	}
 
 	@Override
-	public double rechargeAccount(String mobileNo, double rechargeAmount) {
+	public double rechargeAccount(String mobileNo, double rechargeAmount) throws PhoneNumberNotFoundException {
 		double d = dao.rechargeAccount(mobileNo, rechargeAmount);
 		return d;
 	}
