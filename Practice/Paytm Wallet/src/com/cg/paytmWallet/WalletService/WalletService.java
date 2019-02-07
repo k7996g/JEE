db4c35d4f567eb85ccff033e8487185a989a1225
k7996g <35267235+k7996g@users.noreply.github.com>
2019-02-07 17:44:16 +0530

    Delete WalletService.java

--- Practice/Paytm Wallet/src/com/cg/paytmWallet/WalletService/WalletService.java
+++
@@ -1,8 +0,0 @@
-package com.cg.paytmWallet.WalletService;
-
-import java.math.BigDecimal;
-
-public interface WalletService {
-void createAccount(String name,String phone,double d);
-void showBalance(String phone);
-}
