c3a691d5546b10d29f5e67fceabfad08898315f9
k7996g <35267235+k7996g@users.noreply.github.com>
2019-02-07 08:18:59 -0800

    Update AccountDao.java

--- Exam Question/src/com/cg/mra/dao/AccountDao.java
+++ Exam Question/src/com/cg/mra/dao/AccountDao.java
@@ -1,9 +1,10 @@
 package com.cg.mra.dao;
 
 import com.cg.mra.bean.Account;
+import com.cg.mra.exception.PhoneNumberNotFoundException;
 
 public interface AccountDao {
-	Account getAccountDetails(String mobileNo);
+	Account getAccountDetails(String mobileNo) throws PhoneNumberNotFoundException;
 
-	double rechargeAccount(String mobileNo, double rechargeAmount);
+	double rechargeAccount(String mobileNo, double rechargeAmount) throws PhoneNumberNotFoundException;
 }
